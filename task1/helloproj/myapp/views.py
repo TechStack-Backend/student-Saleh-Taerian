2688d6a1f1d81219091b43f2a1d584d12789db56
saleh <saleh1321385@gmail.com>
2025-08-14 23:56:48 +0330

    <feat>(task-1): returning http response and connecting to urls

--- task1/helloproj/myapp/views.py
+++ task1/helloproj/myapp/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render , HttpResponse
 
 # Create your views here.
+def  returnHello(requset):
+    return HttpResponse("<h1>Hello World Saleh Taerian</h1>")

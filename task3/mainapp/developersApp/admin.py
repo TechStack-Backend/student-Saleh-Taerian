79111eaddb735ce4cee9210bca64aca76f2a565d
saleh <saleh1321385@gmail.com>
2025-09-15 20:19:31 +0330

    feat (task-3): admin panel created and db added to adminpanel

--- task3/mainapp/developersApp/admin.py
+++ task3/mainapp/developersApp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Project , Developers , Skill
 # Register your models here.
+admin.site.register(Project)
+admin.site.register(Developers)
+admin.site.register(Skill)

1091592cbb7669b4bcbf2c1978e2897a9c549529
saleh <saleh1321385@gmail.com>
2025-09-19 02:08:04 +0330

    feat (task-3):model and form is ready

--- task3/mainapp/developersApp/urls.py
+++ task3/mainapp/developersApp/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from .views import show_sudents
+from .views import add_developer , add_proj , add_skl ,show_developers ,show_projects
+
 urlpatterns = [
-    path("developers/" , show_sudents)
+    path('login/dev' , add_developer),
+    path('login/skill' ,add_skl) , 
+    path('login/project' , add_proj),
+    path('developers' , show_developers),
+    path('projects' , show_projects)
 ]

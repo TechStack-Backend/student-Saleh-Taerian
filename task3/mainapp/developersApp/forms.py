da512f60e04397ce9e5aef69d58d8ef2948a2d75
saleh <saleh1321385@gmail.com>
2025-10-31 17:13:32 +0330

    <chore> task-3 : changing FBV to CBV

--- task3/mainapp/developersApp/forms.py
+++ task3/mainapp/developersApp/forms.py
@@ -1,21 +1,35 @@
 from django import forms
 from .models import Developer
+from django.forms import ModelForm
+from .models import Developer, Skill, Project
 
-class developer_login(forms.Form):
-    first_name = forms.CharField(max_length=100)
-    last_name = forms.CharField(max_length=100)
-    email = forms.CharField(max_length=100)
-    age = forms.IntegerField()
+class developer_login(ModelForm):
+    class Meta():
+        model = Developer
+        fields =["first_name" , "last_name" , "email" , "age"]
+# class developer_login(forms.Form):
+#     first_name = forms.CharField(max_length=100)
+#     last_name = forms.CharField(max_length=100)
+#     email = forms.CharField(max_length=100)
+#     age = forms.IntegerField()
     
     
-class add_project(forms.Form):
-    title = forms.CharField(max_length=100)
-    description = forms.CharField(max_length=100)
-    developers = forms.ModelMultipleChoiceField(queryset=Developer.objects.all() , widget = forms.CheckboxSelectMultiple)
+class add_project(ModelForm):
+    class Meta():
+        model = Project
+        fields = ["title" , "description" , "developers"]
+        widgets = {'developers': forms.CheckboxSelectMultiple() }
+# class add_project(forms.Form):
+#     title = forms.CharField(max_length=100)
+#     description = forms.CharField(max_length=100)
+#     developers = forms.ModelMultipleChoiceField(queryset=Developer.objects.all() , widget = forms.CheckboxSelectMultiple)
     
-    
-class add_skill(forms.Form):
-    title = forms.CharField(max_length=100)
-    description = forms.CharField(max_length=100)
-    developer  = forms.ModelChoiceField(queryset= Developer.objects.all())
+class add_skill(ModelForm):
+    class Meta():
+        model = Skill
+        fields = ["title" , "description" ,"developer"]
+# class add_skill(forms.Form):
+#     title = forms.CharField(max_length=100)
+#     description = forms.CharField(max_length=100)
+#     developer  = forms.ModelChoiceField(queryset= Developer.objects.all())
     
--- task3/mainapp/developersApp/urls.py
+++ task3/mainapp/developersApp/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from .views import add_developer , add_proj , add_skl ,show_developers ,show_projects
-
+from .views import projectCreateView , developerCreateView , developerListView , projectListview , skillCreateView , developerDetail
 urlpatterns = [
-    path('login/dev' , add_developer),
-    path('login/skill' ,add_skl) , 
-    path('login/project' , add_proj),
-    path('developers' , show_developers),
-    path('projects' , show_projects)
+    path('login/dev' , developerCreateView.as_view() , name = "addDev"),
+    path('login/skill' ,skillCreateView.as_view() , name="addSkill") , 
+    path('login/project' , projectCreateView.as_view() , name="addProj"),
+    path('developers' , developerListView.as_view() , name="ShowDevs"),
+    path('projects' , projectListview.as_view() , name="showProjs"),
+    path('eachDev/<int:pk>' , developerDetail.as_view() , name ="showEachDev")
 ]
